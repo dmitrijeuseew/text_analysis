500ee0c296253533d8802bc4993855d89432babe
dmitry <dmitrij.euseew@yandex.ru>
2023-10-31 12:27:43 +0300

    update topics models

--- services/topics/server.py
+++ services/topics/server.py
@@ -18,14 +18,22 @@ class Payload(BaseModel):
 
 topic_cls = build_model("topic_cls_chatgpt_22.json", download=True)
 subtopic_cls = build_model("topic_cls_chatgpt_120.json", download=True)
-subtopic_health = build_model("topic_cls_health.json", download=True)
+topics2_health = build_model("topics2_health.json", download=True)
+topics3_health = build_model("topics3_health.json", download=True)
 
-subtopics_list = []
-with open("/data/models/classifiers/subtopic_healthcare_cls/classes.dict", 'r') as inp:
+topics2_h_list = []
+with open("/data/models/classifiers/topics2_healthcare/classes.dict", 'r') as inp:
     lines = inp.readlines()
     for line in lines:
         line_split = line.strip().split("\t")
-        subtopics_list.append(line_split[0])
+        topics2_h_list.append(line_split[0])
+
+topics3_h_list = []
+with open("/data/models/classifiers/topics3_healthcare/classes.dict", 'r') as inp:
+    lines = inp.readlines()
+    for line in lines:
+        line_split = line.strip().split("\t")
+        topics3_h_list.append(line_split[0])
 
 file_subtopics = "/data/downloads/topic_subtopics.json"
 subtopics_dict = json.load(open(file_subtopics))
@@ -64,6 +72,8 @@ async def model(payload: Payload):
             f_texts, nf_texts = [], []
             cur_texts = texts[i*batch_size:(i+1)*batch_size]
             for n, text in enumerate(cur_texts):
+                if text is None:
+                    text = ""
                 f_text, found = preprocess_text(text)
                 if found:
                     nf_texts.append([f_text, n])
@@ -75,28 +85,39 @@ async def model(payload: Payload):
             if raw_texts:
                 labels = topic_cls(raw_texts)
                 labels_sp = subtopic_cls(raw_texts)
-                labels_h, probas_h = subtopic_health(raw_texts)
+                labels2_h, probas2_h = topics2_health(raw_texts)
+                labels3_h, probas3_h = topics3_health(raw_texts)
 
-                for nl, (label, label_h, proba_h, label_sp) in enumerate(zip(labels, labels_h, probas_h, labels_sp)):
-                    label_probas_h = zip(subtopics_list, proba_h)
-                    label_probas_h = sorted(label_probas_h, key=lambda x: x[1], reverse=True)
-                    if label_probas_h[0][1] < 0.9 and label != "Здравоохранение":
-                        label_h = "Не найдено"
-                    f_labels.append([label, label_h, label_sp, f_texts[nl][1]])
-                    logger.info(f"labels: {label} --- {labels_h}")
+                for nl, (label, label2_h, proba2_h, label3_h, proba3_h, label_sp) in \
+                        enumerate(zip(labels, labels2_h, probas2_h, labels3_h, probas3_h, labels_sp)):
+                    label2_probas_h = zip(topics2_h_list, proba2_h)
+                    label2_probas_h = sorted(label2_probas_h, key=lambda x: x[1], reverse=True)
+                    if label2_probas_h[0][1] < 0.92 and label != "Здравоохранение":
+                        label2_h = "Не найдено"
+
+                    label3_probas_h = zip(topics3_h_list, proba3_h)
+                    label3_probas_h = sorted(label3_probas_h, key=lambda x: x[1], reverse=True)
+                    if label2_h == "Не найдено":
+                        label3_h = "Не найдено"
+                    
+                    with open("/src/log.txt", 'a') as out:
+                        out.write("label2 "+str(label2_probas_h[:3])+'\n')
+                        out.write("label3 "+str(label3_probas_h[:3])+'\n')
+                        out.write(f"label: {label}"+'\n\n')
+                    f_labels.append([label, label2_h, label3_h, label_sp, f_texts[nl][1]])
 
             for _, n in nf_texts:
-                nf_labels.append(["Не найдено", "Не найдено", ["Не найдено"], n])
+                nf_labels.append(["Не найдено", "Не найдено", "Не найдено", ["Не найдено"], n])
 
             total_labels = f_labels + nf_labels
             total_labels = sorted(total_labels, key=lambda x: x[-1])
-            cur_twl = [[text, element[0], element[1], element[2]] for text, element in zip(cur_texts, total_labels)]
+            cur_twl = [[text, element[0], element[1], element[2], element[3]] for text, element in zip(cur_texts, total_labels)]
 
             new_cur_twl = []
-            for text, label, label_h, label_sp in cur_twl:
-                if label_h != "Не найдено":
+            for text, label, label2_h, label3_h, label_sp in cur_twl:
+                if label2_h != "Не найдено":
                     label = "Здравоохранение"
-                    label_sp = label_h
+                    label_sp = label2_h
                 elif label == "Не найдено":
                     label_sp = "Не найдено"
                 elif label_sp[0] == "Не найдено":
@@ -114,13 +135,16 @@ async def model(payload: Payload):
                 else:
                     label = "Не найдено"
                     label_sp = "Не найдено"
-                processed_elements.append({"text": text, "topic": label, "subtopic": label_sp})
+
+                processed_elements.append({"text": text, "topic1": label, "topic2": label_sp, "topic3": label3_h})
         except Exception as e:
-            logger.info(f"error: {e}")
+            print(f"error: {e}")
             cur_texts = texts[i*batch_size:(i+1)*batch_size]
             for text in cur_texts:
-                processed_elements.append({"text": text, "topic": "Не найдено", "subtopic": "Не найдено"})
-
+                processed_elements.append({"text": text,
+                                           "topic1": "Не найдено",
+                                           "topic2": "Не найдено",
+                                           "topic3": "Не найдено"})
     return processed_elements
 
 
